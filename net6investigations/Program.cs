19ce497d9b972ce7e1bb18f56c1f0b5658770f90
Carsten Wollsen <carsten.wollsen@gmail.com>
2015-12-07 14:13:03 +0100

    nameof operator

--- net6investigations/Program.cs
+++ net6investigations/Program.cs
@@ -8,14 +8,36 @@ namespace net6investigations
 {
 	class Program
 	{
+		private static List<MyPoint> points;
+
 		static void Main(string[] args)
 		{
-			var p = new MyPoint(5 ,6);
+			points = new List<MyPoint>();
+
+			var p = new MyPoint(5, 6);
+			MyPoint q = null;
+
+
+			AddPoint(p);
+
+			AddPoint(q);
+
 
 			Console.WriteLine(p.ToString());
 			Console.WriteLine(p.Distance);
 
 			Console.ReadKey();
+		}
+
+
+		public static void AddPoint(MyPoint myPoint)
+		{
+			if (myPoint == null)
+			{
+				throw new ArgumentNullException(nameof(myPoint));
+			}
+
+			points.Add(myPoint);
 		}
 	}
 }
