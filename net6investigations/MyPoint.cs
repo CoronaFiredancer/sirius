9806e9cb527c790cdfa6c16bcb5f186692f865d6
Carsten Wollsen <carsten.wollsen@gmail.com>
2015-12-07 13:12:08 +0100

    Using string interpolation and expression bodied methods and properties

---
+++ net6investigations/MyPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace net6investigations
+{
+	public class MyPoint
+	{
+		public int X { get; }
+		public int Y { get; }
+
+		public MyPoint(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public double Distance => Sqrt(X*X + Y*Y);
+
+		public override string ToString() => $"{X}, {Y}";
+	}
+}
--- net6investigations/Program.cs
+++ net6investigations/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace net6investigations
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var p = new MyPoint(5 ,6);
+
+			Console.WriteLine(p.ToString());
+			Console.WriteLine(p.Distance);
+
+			Console.ReadKey();
+		}
+	}
 }
